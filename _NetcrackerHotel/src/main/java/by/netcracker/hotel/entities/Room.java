7ab5f73eae012c166cc1164cf15ff365355fc400
Roman Rodevich <romano-hi@mail.ru>
2017-03-25 21:52:21 +0300

    Создана основа для главной страницы (home.jsp),
     подключена таблица стилей (home1.css),
     созданы первые классы User, Room (package entities)

---
+++ _NetcrackerHotel/src/main/java/by/netcracker/hotel/entities/Room.java
@@ -0,0 +1,42 @@
+package by.netcracker.hotel.entities;
+
+public class Room {
+
+	private int id;
+	private int cost;
+	private int clas;
+	private int capacity;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getCost() {
+		return cost;
+	}
+
+	public void setCost(int cost) {
+		this.cost = cost;
+	}
+
+	public int getClas() {
+		return clas;
+	}
+
+	public void setClas(int clas) {
+		this.clas = clas;
+	}
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+
+}
---
+++ _NetcrackerHotel/src/main/java/by/netcracker/hotel/entities/User.java
@@ -0,0 +1,69 @@
+package by.netcracker.hotel.entities;
+
+public class User {
+
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String login;
+	private String password;
+	private String email;
+	private int accessLevel;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getAccessLevel() {
+		return accessLevel;
+	}
+
+	public void setAccessLevel(int accessLevel) {
+		this.accessLevel = accessLevel;
+	}
+
+}
