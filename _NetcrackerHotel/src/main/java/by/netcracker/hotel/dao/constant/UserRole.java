380c33816c52ec0390a93f03dac7b2c1fcdf65a8
hotkovycheslav <hotkovycheslav@gmail.com>
2017-04-11 02:45:38 +0300

    UserRole enum

--- _NetcrackerHotel/src/main/java/by/netcracker/hotel/dao/constant/UserRole.java
+++ _NetcrackerHotel/src/main/java/by/netcracker/hotel/dao/constant/UserRole.java
@@ -4,7 +4,7 @@ package by.netcracker.hotel.dao.constant;
  * Created by slava on 11.04.17.
  */
 public enum UserRole {
-    ROLE_USER, ROLE_ADMIN;
+    GUEST,USER, ADMIN;
 
     public final String getRole(){
         return this.name();
